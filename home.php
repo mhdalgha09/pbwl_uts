c6fd8fa896c8050d7d56def6d07f3a763dc9b014
mhdalgha09 <alghazaliii01@gmail.com>
2022-06-24 21:28:51 -0700

    first

---
+++ home.php
@@ -0,0 +1,198 @@
+<!-- ======= Hero Section ======= -->
+<section id="hero">
+    <div id="heroCarousel" data-bs-interval="5000" class="carousel slide carousel-fade" data-bs-ride="carousel">
+
+      <ol class="carousel-indicators" id="hero-carousel-indicators"></ol>
+
+      <div class="carousel-inner" role="listbox">
+
+        <!-- Slide 1 -->
+        <div class="carousel-item active" style="background-image: url(assets/img/slide/slide1.webp)">
+        </div>
+
+        <!-- Slide 2 -->
+        <div class="carousel-item" style="background-image: url(assets/img/slide/slide2.jpg)">
+        </div>
+
+        <!-- Slide 3 -->
+        <div class="carousel-item" style="background-image: url(assets/img/slide/slide3.jpg)">
+        </div>
+
+      </div>
+
+      <a class="carousel-control-prev" href="#heroCarousel" role="button" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon bi bi-chevron-left" aria-hidden="true"></span>
+      </a>
+
+      <a class="carousel-control-next" href="#heroCarousel" role="button" data-bs-slide="next">
+        <span class="carousel-control-next-icon bi bi-chevron-right" aria-hidden="true"></span>
+      </a>
+
+    </div>
+  </section><!-- End Hero -->
+
+<section id="about" class="about">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+          <h2>Tentang SMAN 1 SIPISPIS</h2>
+          <p> <b> SMAN 1 SIPISPIS </b>adalah salah satu satuan pendidikan dengan jenjang SMA di SIPISPIS, Kec. SIPISPIS, Kab. Serdang Bedagai, Sumatera Utara. Dalam menjalankan kegiatannya, SMAN 1 SIPISPIS berada di bawah naungan Kementerian Pendidikan dan Kebudayaan..</p>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-6" data-aos="fade-right">
+            <img src="assets/img/slide/slide3.jpg" class="img-fluid" alt="">
+          </div>
+          <div class="col-lg-6 pt-4 pt-lg-0 content" data-aos="fade-left">
+            <h3>FASILITAS YANG DISEDIAKAN SMAN 1 SIPISPIS</h3>
+            <p>
+            SMAN 1 SIPISPIS menyediakan listrik untuk membantu kegiatan belajar mengajar. Sumber listrik yang digunakan oleh SMAN 1 HALONGONAN berasal dari PLN. SMAN 1 HALONGONAN menyediakan akses internet yang dapat digunakan untuk mendukung kegiatan belajar mengajar menjadi lebih mudah. 
+            Provider yang digunakan SMAN 1 SIPISPIS untuk sambungan internetnya adalah Telkomsel Flash dan juga XL.
+            </p>
+            <h3>JAM PEMBELAJARAN DI SMAN 1 SIPISPIS</h3>
+            <p>Pembelajaran di SMAN 1 SIPISPIS dilakukan pada Pagi. Dalam seminggu, pembelajaran dilakukan dari Senin sampai dengan Sabtu.</p>
+            <h3>AKREDITASI</h3>
+            <p>SMAN 1 SIPISPIS memiliki akreditasi A, berdasarkan sertifikat 742/BAP-SM/LL/X/2016.</p>
+          </div>
+            <div class="col-lg-6 pt-4 pt-lg-0 content">
+            <ul>
+              <h3>Identitas Satuan Pendidikan</h3>
+              <li><i class="bi bi-check-circle"></i>Nama    : SMAN 1 SIPISPIS</li>
+              <li><i class="bi bi-check-circle"></i>NPSN    : 10209395</li>
+              <li><i class="bi bi-check-circle"></i>Alamat  :	SIPISPIS</li>
+              <li><i class="bi bi-check-circle"></i>Kode Pos : 20992</li>
+              <li><i class="bi bi-check-circle"></i>Kelurahan : Buluh Duri</li>
+              <li><i class="bi bi-check-circle"></i>Kecamatan / Kota (LN)	: Kec. Sipispis</li>
+              <li><i class="bi bi-check-circle"></i>Kab : Kab. Serdang Bedagai</li>
+              <li><i class="bi bi-check-circle"></i>Provinsi : Sumatera Utara</li>
+              <li><i class="bi bi-check-circle"></i>Status Sekolah : Negeri</li>
+              <li><i class="bi bi-check-circle"></i>Jenjang Pendidikan : SMA</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section><!-- End About Us Section -->
+
+    <!-- ======= Counts Section ======= -->
+    <section id="counts" class="counts">
+      <div class="container" data-aos="fade-up">
+
+        <div class="row no-gutters">
+
+          <div class="col-lg-3 col-md-6 d-md-flex align-items-md-stretch">
+            <div class="count-box">
+              <i class="fa fa-user"></i>
+              <span data-purecounter-start="0" data-purecounter-end="200" data-purecounter-duration="1" class="purecounter"></span>
+              <br>
+              <strong>Siswa</strong>
+              <a href="#">Find out more &raquo;</a>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-6 d-md-flex align-items-md-stretch">
+            <div class="count-box">
+              <i class="fa fa-user"></i>
+              <span data-purecounter-start="0" data-purecounter-end="150" data-purecounter-duration="1" class="purecounter"></span>
+              <br>
+              <strong>Siswi</strong> 
+              <a href="#">Find out more &raquo;</a>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-6 d-md-flex align-items-md-stretch">
+            <div class="count-box">
+              <i class="fa fa-graduation-cap"></i>
+              <span data-purecounter-start="0" data-purecounter-end="3500" data-purecounter-duration="1" class="purecounter"></span>
+              <br>
+              <strong>Alumni</strong>
+              <a href="#">Find out more &raquo;</a>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-6 d-md-flex align-items-md-stretch">
+            <div class="count-box">
+              <i class="fas fa-award"></i>
+              <span>A</span>
+              <br>
+              <strong>Akreditasi</strong>
+              <a href="#">Find out more &raquo;</a>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Counts Section -->
+
+    <!-- ======= Contact Section ======= -->
+    <section id="contact" class="contact">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>Kontak</h2>
+          <p>SMAN 1 SIPISPIS berada di koordinat Garis lintang: 3.2009 dan Garis bujur: 99.0439.</p>
+        </div>
+
+      </div>
+
+      <div>
+      <iframe style="border:0; width: 100%; height: 350px;"src=https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3983.5926990317744!2d99.04158221373564!3d3.2011712537258523!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3031510ecb7aa557%3A0x5b7f4ff494bd2000!2sSMAN%201%20Sipispis!5e0!3m2!1sid!2sid!4v1652844906446!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+      </div>
+
+      <div class="container">
+
+        <div class="row mt-5">
+
+          <div class="col-lg-6">
+
+            <div class="row">
+              <div class="col-md-12">
+                <div class="info-box">
+                  <i class="bx bx-map"></i>
+                  <h3>Alamat</h3>
+                  <p>Buluh Duri Kec. Sipispis Kab. Serdang Bedagai</p>
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="info-box mt-4">
+                  <i class="bx bx-envelope"></i>
+                  <h3>Email</h3>
+                  <p>salmanbahresi99@yahoo.com</p>
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="info-box mt-4">
+                  <i class="bx bx-phone-call"></i>
+                  <h3>Telepon</h3>
+                  <p>+628 1378 0756 98</p>
+                </div>
+              </div>
+            </div>
+
+          </div>
+
+          <div class="col-lg-6">
+            <form action="#" method="post" role="form" class="php-email-form">
+              <div class="row">
+                <div class="col form-group">
+                  <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" required>
+                </div>
+              <div class="form-group mt-3">
+                <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" required>
+              </div>
+              <div class="form-group mt-3">
+                <textarea class="form-control" name="message" rows="5" placeholder="Message" required></textarea>
+              </div>
+              <div class="my-3">
+                <div class="loading">Loading</div>
+                <div class="error-message"></div>
+                <div class="sent-message">Your message has been sent. Thank you!</div>
+              </div>
+              <div class="text-center"><button type="submit">Send Message</button></div>
+            </form>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Contact Section -->
